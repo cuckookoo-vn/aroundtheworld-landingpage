1c7176c4da8aeaae6206d3fd180132d9da88ada7
Roots <roots.phan@cuckookoo.vn>
2022-09-24 09:40:55 +0700

    Root : 'finished header for pc'

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,11 +1,11 @@
 import './header.scss'
 import {useState} from "react";
 
-
 const Header = () =>{
     const [statusDocs, setStatusDoc] = useState(false);
     const [statusLang, setStatusLang] = useState(false);
     const [textLang, setTextLang] = useState('Eng');
+
     const images = {
         bgHeader: process.env.PUBLIC_URL + '/images/bg-header.png',
         titleHeader : process.env.PUBLIC_URL + "/images/title-header.png",
@@ -30,53 +30,64 @@ const Header = () =>{
         setStatusLang(false);
     }
 
-    window.onclick = function (){
-        setStatusMenu(true,'full')
+    window.onclick = (event) => {
+        if (
+            !event.target.matches('.drop-btn-docs') &&
+            !event.target.matches('.icon-select-header') &&
+            !event.target.matches('.drop-btn-lang')
+        ){
+            setStatusMenu(true,'full')
+        }
     }
 
     return(
         <div className="header"
-             style={{backgroundImage:`url(${images.bgHeader})`}}
-        >
-            <img
-                className="title-header"
-                src={images.titleHeader}
-                alt="title-header"
-            />
-            <img
-                className="logo-header"
-                src={images.logoHeader}
-                alt="logo-header"
-            />
+             style={{backgroundImage:`url(${images.bgHeader})`}}>
+
+            <img className="title-header"
+                 src={images.titleHeader}
+                 alt="title-header"/>
+            <img className="logo-header"
+                 src={images.logoHeader}
+                 alt="logo-header"/>
 
             <div className="dropdown dropdown-docs">
-                <button className="drop-btn drop-btn-docs" onClick={()=>setStatusMenu(statusDocs, 'lang')}>
+                <button className="drop-btn drop-btn-docs"
+                        onClick={()=>setStatusMenu(statusDocs, 'docs')}>
                     Docs
-                    <img src={images.iconSelect} alt="icon-select"/>
+                    <img className="icon-select-header"
+                         src={images.iconSelect} alt="icon-select"/>
                 </button>
-                <div className="dropdown-content dropdown-content-docs">
-                    <a className="drop-option" href="#">Pitch Deck</a>
+
+                <div className={"dropdown-content dropdown-content-docs" + (statusDocs ? " show": "")}>
+                    <a className="drop-option"
+                       href={"https://www.youtube.com/"} >Pitch Deck</a>
                 </div>
             </div>
 
-            <div className="dropdown dropdown-eng">
-                <button className="drop-btn drop-btn-eng" onClick={()=>setStatusMenu(statusLang, 'lang')}>
+            <div className="dropdown dropdown-lang">
+                <button className="drop-btn drop-btn-lang"
+                        onClick={()=>setStatusMenu(statusLang, 'lang')}>
                     {textLang}
-                    <img src={images.iconSelect} alt="icon-select"/>
+                    <img className="icon-select-header"
+                         src={images.iconSelect} alt="icon-select"/>
                 </button>
-                <div className="dropdown-content dropdown-content-eng">
-                    <button onClick={()=>changeLang('Vn')} className="drop-option">Vietnamese</button>
-                    <button onClick={()=>changeLang('Ko')}className="drop-option">Korean</button>
-                    <button onClick={()=>changeLang('En')} className="drop-option">English</button>
+
+                <div className={"dropdown-content dropdown-content-lang" + (statusLang ? " show": "")}>
+                    <button onClick={()=>changeLang('Vn')}
+                            className="drop-option">Vietnamese</button>
+
+                    <button onClick={()=>changeLang('Ko')}
+                            className="drop-option">Korean</button>
+
+                    <button onClick={()=>changeLang('En')}
+                            className="drop-option">English</button>
                 </div>
             </div>
 
             <div className="logo-coming-soon"
-                 style={{backgroundImage:`url(${images.comingSoonButton}`}}
-            >
-                <span>
-                    COMING SOON
-                </span>
+                 style={{backgroundImage:`url(${images.comingSoonButton}`}}>
+                <span>COMING SOON</span>
             </div>
         </div>
     )
